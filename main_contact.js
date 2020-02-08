1fac591116a44eb3a12ead0637ee4832d86bd26d
katablade <57857686+katablade@users.noreply.github.com>
2020-02-08 13:46:47 +0100

    Add files via upload

---
+++ main_contact.js
@@ -0,0 +1,17 @@
+function eror(){
+    if(document.getElementById("firstname").value==""){
+        alert("please check your first name")
+    }
+    else if(document.getElementById("lastname").value==""){
+        alert("please check your last name")
+    }
+    else if(document.getElementById("number").value==""){
+        alert("please check your phone number")
+    }
+    else if(document.getElementById("email").value==""){
+        alert("please check your email address")
+    }
+    else if(document.getElementById("password").value==""){
+        alert("please check your password")
+    }
+}
